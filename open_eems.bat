5b2c82d8c9d12e7edd2fe108537a921da009a844
Rajalingam Muthiah <131362761+RajalingamMuthiah@users.noreply.github.com>
2025-12-29 16:01:08 +0000

    Success

--- open_eems.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-REM Quick access to EEMS application
-start http://localhost:8080/eems/
--- open_phpmyadmin.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-REM Quick access to phpMyAdmin
-start http://localhost:8080/phpmyadmin/
